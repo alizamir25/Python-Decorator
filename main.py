fc9480076f7404d15d447382f9a42a9513f302a5
alizamir25 <ali0786zamir@gmail.com>
2024-08-04 14:13:55 +0100

    Patch
    Code clean up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
 
-
 def new_decorator(func):
     def wrapper():
         tic = time.perf_counter()
@@ -9,17 +8,14 @@ def new_decorator(func):
         print(f"Runtime:{toc - tic:0.7f} seconds")
     return wrapper
 
-
 @new_decorator
 def print_hello_world():
     print("Hello World")
-
 
 @new_decorator
 def print_hello_world_longer():
     time.sleep(0.25)
     print("Hello World")
-
 
 print_hello_world()
 print_hello_world_longer()
